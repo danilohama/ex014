b404bcc0cd1d783caddd23fdde0b9c5ac4af09fc
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-05 20:05:36 -0300

    Add files via upload

---
+++ ex014.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que le a temperatura em °C
+
+temp = float(input('Informe a temperatura em °C: '))
+f = ((9 * temp / 5) + 32)
+print('A temperatura de {}°C Corresponde a {}°F'.format(temp, f))
